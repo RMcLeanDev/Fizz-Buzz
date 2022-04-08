46cce8b778bd636b32197ac3623fce2eb06743ef
Ryan <ryan@MEME-TIME.local>
2022-04-08 13:02:23 -0700

    add conditionals inside function for number input and push fizz, buzz, fizz-buzz to array

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ function App() {
 
   const [numInput, setNumInput] = useState(0);
   const [error, setError] = useState(null);
+  const result = [];
 
   function startFizzBuzz(e){
     setError(null);
     e.preventDefault();
+    let number = parseInt(numInput)
     if(numInput === 0){
       setError("Number must be greater than 0")
     } else {
-      
+      console.log(10%3)
+      for(let i=0; i<5; i++){
+        if(number%3 === 0 && number%5 === 0){
+          result.push("fizz-buzz")
+        } else if(number%5 === 0){
+          result.push("buzz")
+        } else if(number%3 === 0){
+          result.push("fizz")
+        } else {
+          result.push(number)
+        }
+        number ++;
+      }
+      console.log(result);
     }
   }
 
