0ed5e8ecf3469d7d89b6558e5ae91cdff79d59a7
Ryan <ryan@MEME-TIME.local>
2022-04-07 20:56:46 -0700

    add input and form into App and create startFizzBuzz function. starting conditionals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.scss';
 
 function App() {
+
+  const [numInput, setNumInput] = useState(0);
+  const [error, setError] = useState(null);
+
+  function startFizzBuzz(e){
+    setError(null);
+    e.preventDefault();
+    if(numInput === 0){
+      setError("Number must be greater than 0")
+    }
+  }
+
   return (
     <div className="App">
       <h1>Fizz-Buzz</h1>
+      {error}
+      <form onSubmit={startFizzBuzz}>
+        <input type="number" value={numInput} onChange={e => setNumInput(e.target.value)}/>
+      </form>
     </div>
   );
 }
